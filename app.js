69272ab8fe1e3f725167046fa38e1df6fd2604cf
akshayn25 <100288356+akshayn25@users.noreply.github.com>
2022-09-15 16:56:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ class UI{
                           <i class="fas fa-chevron-down" data-id=${item.id}></i>
                       </div> `;
       cartContent.appendChild(div);
-      document.cookie=item.title+"="+item.price+"; expires=Wed, 15 Sep 2022 23:00:00 UTC; path=/";
+      document.cookie=item.title+"="+item.price+"; expires=Thu, 15 Sep 2022 23:00:00 UTC; path=/";
   }
    showCart(){
      cartOverlay.classList.add('transparentBcg') ;
